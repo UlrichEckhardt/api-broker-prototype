35637d162427e0cf3944ebc31a4e84f6c2ee080a
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2019-12-26 11:42:40 +0100

    Add a script to start up environment (MongoDB, Network) in Docker.

---
+++ startup.sh
@@ -0,0 +1,13 @@
+# name for the Docker network
+BROKER_NETWORK=api-broker-prototype
+
+# image name for the MongoDB server
+MONGODB_IMAGE=mongo:4.2
+
+docker network create $BROKER_NETWORK
+
+# create MongoDB server
+docker run --rm --detach --name mongodb --network $BROKER_NETWORK $MONGODB_IMAGE
+sleep 2 # Hack: wait for the DB to start up
+docker exec mongodb mongo --eval "db.createCollection('events')"
+docker exec mongodb mongo --eval "db.createCollection('notifications', {capped:true, size: 1000000})"
