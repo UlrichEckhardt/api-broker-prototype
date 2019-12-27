855c1f655235b257d91304fb8e940b5b0876b1fe
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2019-12-27 11:40:28 +0100

    startup with mongodb port forwarded to localhost

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ MONGODB_IMAGE=mongo:4.2
 docker network create $BROKER_NETWORK
 
 # create MongoDB server
-docker run --rm --detach --name mongodb --network $BROKER_NETWORK $MONGODB_IMAGE
+docker run --rm --detach --name mongodb --network $BROKER_NETWORK --publish 27017:27017 $MONGODB_IMAGE
 sleep 2 # Hack: wait for the DB to start up
 docker exec mongodb mongo --eval "db.createCollection('events')"
 docker exec mongodb mongo --eval "db.createCollection('notifications', {capped:true, size: 1000000})"
