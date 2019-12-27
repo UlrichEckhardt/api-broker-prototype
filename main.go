4c13608aebd6e413e893c8a0f62975c3b0b1d130
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2019-12-27 11:41:06 +0100

    main.go: Add code to connect to the MongoDB.

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello, 世界")
+	defer fmt.Println("Done.")
+
+	opts := options.Client().ApplyURI("mongodb://localhost").SetAppName("api-broker-prototype").SetConnectTimeout(1 * time.Second)
+	err := opts.Validate()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	client, err := mongo.NewClient(opts)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	if err := client.Connect(ctx); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		fmt.Println(err)
+		return
+	}
 }
