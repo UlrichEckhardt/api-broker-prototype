b2e3a6c98a9003a9a841bba015e47dc91324ad5c
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2020-02-21 22:20:20 +0100

    MongoDB-Codecs: Add codecs.

--- mongodb-codecs_test.go
+++ mongodb-codecs_test.go
@@ -114,3 +114,45 @@ func TestSimpleCodec(t *testing.T) {
 		runTestcase(name, c, &codec, t)
 	}
 }
+
+func TestRequestCodec(t *testing.T) {
+	cases := map[string]testcase{
+		"test request": {
+			event: &requestEvent{request: "some request"},
+			data:  bson.M{"request": "some request"},
+		},
+	}
+
+	var codec requestEventCodec
+	for name, c := range cases {
+		runTestcase(name, c, &codec, t)
+	}
+}
+
+func TestResponseCodec(t *testing.T) {
+	cases := map[string]testcase{
+		"test response": {
+			event: &responseEvent{response: "some response"},
+			data:  bson.M{"response": "some response"},
+		},
+	}
+
+	var codec responseEventCodec
+	for name, c := range cases {
+		runTestcase(name, c, &codec, t)
+	}
+}
+
+func TestFailureCodec(t *testing.T) {
+	cases := map[string]testcase{
+		"test failure": {
+			event: &failureEvent{failure: "some failure"},
+			data:  bson.M{"failure": "some failure"},
+		},
+	}
+
+	var codec failureEventCodec
+	for name, c := range cases {
+		runTestcase(name, c, &codec, t)
+	}
+}
