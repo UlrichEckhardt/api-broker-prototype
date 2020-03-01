1fa3de94b2a9e8b6b0dd3a4bbb5854a6057b9403
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2020-03-01 16:28:39 +0100

    API: Add a feature that allows configuring the stub's behaviour and performance.

--- api.go
+++ api.go
@@ -4,10 +4,39 @@ package main
 // Normally, this would communicate with a service using e.g. HTTP. This code
 // here only serves as example.
 
+import (
+	"errors"
+	"math/rand"
+	"time"
+)
+
+// configuration values
+var conf struct {
+	failureRate float64
+	minDuration float64
+	maxDuration float64
+}
+
+// ConfigureStub can be used to configure the behaviour of the API stub.
+func ConfigureStub(failureRate float64, minDuration, maxDuration float64) {
+	conf.failureRate = failureRate
+	conf.minDuration = minDuration
+	conf.maxDuration = maxDuration
+}
+
 // ProcessRequest handles a single request. It returns a response string on
 // success or an error on failure. Note that similar to the responseEvent, it
 // doesn't distinguish between an answer that signals success or failure so
 // any error here was caused locally.
 func ProcessRequest(request string) (string, error) {
+	// add a random delay
+	delay := time.Duration((conf.minDuration + rand.Float64()*(conf.maxDuration-conf.minDuration)) * float64(time.Second))
+	time.Sleep(delay)
+
+	// fail randomly
+	if rand.Float64() < conf.failureRate {
+		return "", errors.New("failure")
+	}
+
 	return "response", nil
 }
