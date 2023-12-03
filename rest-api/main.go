d8e34af248df62a2bd0e756dc8016b00d003cfda
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2023-12-03 13:28:14 +0100

    rest-api/main.go: Create a template function for REST API handling

---
+++ rest-api/main.go
@@ -0,0 +1,23 @@
+package rest_api
+
+import (
+	"api-broker-prototype/events"
+	"context"
+	"errors"
+)
+
+// serve a simple API to access requests
+func Serve(ctx context.Context, store events.EventStore, port uint) error {
+	if store == nil {
+		return errors.New("the supplied store is nil")
+	}
+	if port == 0 {
+		return errors.New("the supplied port is zero")
+	}
+
+	select {
+	case <-ctx.Done():
+		// cancelled by context
+		return ctx.Err()
+	}
+}
