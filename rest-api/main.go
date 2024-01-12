a8f7a6d68a7a3798033ba34f15b823c0d2ffdc47
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2024-01-12 11:11:21 +0100

    rest-api/main.go: implement a separate controller type

--- rest-api/main.go
+++ rest-api/main.go
@@ -24,67 +24,13 @@ func Serve(ctx context.Context, store events.EventStore, port uint) error {
 	}
 
 	r := gin.Default()
-	r.GET("/up", func(c *gin.Context) {
-		c.String(http.StatusOK, "")
-	})
-	r.HEAD("/up", func(c *gin.Context) {
-		c.String(http.StatusOK, "")
-	})
-	r.GET("/request/:external_uuid", func(c *gin.Context) {
-		// extract external UUID from the path
-		external_uuid := uuid.FromStringOrNil(c.Param("external_uuid"))
-		if external_uuid == uuid.Nil {
-			c.String(http.StatusBadRequest, "Bad Request")
-			return
-		}
-
-		// convert UUID to internal ID
-		id, err := store.ResolveUUID(ctx, external_uuid)
-		if err != nil {
-			c.String(http.StatusNotFound, "failed to resolve external UUID")
-			return
-		}
-
-		// load event
-		envelope, err := store.RetrieveOne(ctx, id)
-		if err != nil {
-			c.String(http.StatusInternalServerError, "failed to load event")
-			return
-		}
-
-		response := newResponseDTO(envelope)
-		c.JSON(http.StatusOK, response)
-	})
-	r.POST("/request/:external_uuid", func(c *gin.Context) {
-		// extract external UUID from the path
-		external_uuid := uuid.FromStringOrNil(c.Param("external_uuid"))
-		if external_uuid == uuid.Nil {
-			c.String(http.StatusBadRequest, "Bad Request")
-			return
-		}
-
-		// extract event from the body
-		request := requestDTO{}
-		if err := c.BindJSON(&request); err != nil {
-			c.String(http.StatusBadRequest, "Bad Request")
-			return
-		}
-
-		// insert event
-		envelope, err := store.Insert(ctx, external_uuid, request.asEvent(), 0)
-		if err != nil {
-			if err == events.DuplicateEventUUID {
-				c.String(http.StatusConflict, "Conflict")
-				return
-			}
-
-			c.String(http.StatusInternalServerError, "Internal Server Error")
-			return
-		}
-
-		response := newResponseDTO(envelope)
-		c.JSON(http.StatusCreated, response)
-	})
+	ctrl := requestController{
+		store: store,
+	}
+	r.GET("/up", ctrl.up)
+	r.HEAD("/up", ctrl.up)
+	r.GET("/request/:external_uuid", ctrl.getRequest)
+	r.POST("/request/:external_uuid", ctrl.postRequest)
 
 	listener, err := net.Listen("tcp", fmt.Sprint(":", port))
 	if err != nil {
@@ -103,6 +49,71 @@ func Serve(ctx context.Context, store events.EventStore, port uint) error {
 		// cancelled by context
 		return ctx.Err()
 	}
+}
+
+type requestController struct {
+	store events.EventStore
+}
+
+func (ctrl *requestController) up(c *gin.Context) {
+	c.String(http.StatusOK, "")
+}
+
+func (ctrl *requestController) getRequest(c *gin.Context) {
+	// extract external UUID from the path
+	external_uuid := uuid.FromStringOrNil(c.Param("external_uuid"))
+	if external_uuid == uuid.Nil {
+		c.String(http.StatusBadRequest, "Bad Request")
+		return
+	}
+
+	// convert UUID to internal ID
+	id, err := ctrl.store.ResolveUUID(c.Request.Context(), external_uuid)
+	if err != nil {
+		c.String(http.StatusNotFound, "failed to resolve external UUID")
+		return
+	}
+
+	// load event
+	envelope, err := ctrl.store.RetrieveOne(c.Request.Context(), id)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "failed to load event")
+		return
+	}
+
+	response := newResponseDTO(envelope)
+	c.JSON(http.StatusOK, response)
+}
+
+func (ctrl *requestController) postRequest(c *gin.Context) {
+	// extract external UUID from the path
+	external_uuid := uuid.FromStringOrNil(c.Param("external_uuid"))
+	if external_uuid == uuid.Nil {
+		c.String(http.StatusBadRequest, "Bad Request")
+		return
+	}
+
+	// extract event from the body
+	request := requestDTO{}
+	if err := c.BindJSON(&request); err != nil {
+		c.String(http.StatusBadRequest, "Bad Request")
+		return
+	}
+
+	// insert event
+	envelope, err := ctrl.store.Insert(c.Request.Context(), external_uuid, request.asEvent(), 0)
+	if err != nil {
+		if err == events.DuplicateEventUUID {
+			c.String(http.StatusConflict, "Conflict")
+			return
+		}
+
+		c.String(http.StatusInternalServerError, "Internal Server Error")
+		return
+	}
+
+	response := newResponseDTO(envelope)
+	c.JSON(http.StatusCreated, response)
 }
 
 // representation of the request during HTTP transfer
