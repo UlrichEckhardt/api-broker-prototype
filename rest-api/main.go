a86fe322a0e50908677a7e262edc98e2d0c83094
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2024-01-12 17:10:45 +0100

    rest-api/main.go: implement a separate controller type

--- rest-api/main.go
+++ rest-api/main.go
@@ -25,69 +25,21 @@ func Serve(ctx context.Context, store events.EventStore, port uint) error {
 
 	e := echo.New()
 
+	ctrl := requestController{
+		store: store,
+	}
 	// GET /up -- server status check
-	e.GET("/up", func(c echo.Context) error {
-		return c.NoContent(http.StatusOK)
-	})
+	e.GET("/up", ctrl.up)
 
 	// HEAD /up -- server status check
-	e.HEAD("/up", func(c echo.Context) error {
-		return c.NoContent(http.StatusOK)
-	})
+	e.HEAD("/up", ctrl.up)
+
+	// GET /request/<external UUID> -- fetch request instance
+	e.GET("/request/:external_uuid", ctrl.getRequest)
 
 	// POST /request/<external UUID> with JSON body -- add a request instance
 	// TODO: Allow an optional UUID which serves as causation ID
-	e.POST("/request/:external_uuid", func(c echo.Context) error {
-		// extract external UUID from the path
-		external_uuid := uuid.FromStringOrNil(c.Param("external_uuid"))
-		if external_uuid == uuid.Nil {
-			return c.String(http.StatusBadRequest, "bad request")
-		}
-
-		// extract event from the body
-		var request requestDTO
-		if err := c.Bind(&request); err != nil {
-			c.Logger().Error(err)
-			return c.String(http.StatusBadRequest, "bad request")
-		}
-
-		// insert event
-		envelope, err := store.Insert(c.Request().Context(), external_uuid, request.asEvent(), 0)
-		if err != nil {
-			c.Logger().Error(err)
-			if err == events.DuplicateEventUUID {
-				return c.String(http.StatusConflict, "duplicate event")
-			}
-			return c.String(http.StatusInternalServerError, "failed to insert new event")
-		}
-
-		response := newResponseDTO(envelope)
-		return c.JSON(http.StatusCreated, response)
-	})
-
-	// GET /request/<external UUID> -- fetch request instance
-	e.GET("/request/:external_uuid", func(c echo.Context) error {
-		// extract external UUID from the path
-		external_uuid := uuid.FromStringOrNil(c.Param("external_uuid"))
-		if external_uuid == uuid.Nil {
-			return c.String(http.StatusBadRequest, "bad request")
-		}
-		e.Logger.Debug("external_uuid=", external_uuid.String())
-
-		id, err := store.ResolveUUID(c.Request().Context(), external_uuid)
-		if err != nil {
-			return c.String(http.StatusNotFound, "failed to resolve external UUID")
-		}
-		e.Logger.Debug("resolved id=", id)
-
-		envelope, err := store.RetrieveOne(c.Request().Context(), id)
-		if err != nil {
-			return c.String(http.StatusInternalServerError, "failed to load event")
-		}
-
-		response := newResponseDTO(envelope)
-		return c.JSON(http.StatusOK, response)
-	})
+	e.POST("/request/:external_uuid", ctrl.postRequest)
 
 	// GET /response/<ID> -- fetch current state of the request <ID>
 	// GET /response -- fetch current state of all requests
@@ -97,6 +49,63 @@ func Serve(ctx context.Context, store events.EventStore, port uint) error {
 
 	e.Logger.SetLevel(log.DEBUG)
 	return e.Start(fmt.Sprint(":", port))
+}
+
+type requestController struct {
+	store events.EventStore
+}
+
+func (ctrl *requestController) up(c echo.Context) error {
+	return c.NoContent(http.StatusOK)
+}
+
+func (ctrl *requestController) getRequest(c echo.Context) error {
+	// extract external UUID from the path
+	external_uuid := uuid.FromStringOrNil(c.Param("external_uuid"))
+	if external_uuid == uuid.Nil {
+		return c.String(http.StatusBadRequest, "bad request")
+	}
+
+	id, err := ctrl.store.ResolveUUID(c.Request().Context(), external_uuid)
+	if err != nil {
+		return c.String(http.StatusNotFound, "failed to resolve external UUID")
+	}
+
+	envelope, err := ctrl.store.RetrieveOne(c.Request().Context(), id)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "failed to load event")
+	}
+
+	response := newResponseDTO(envelope)
+	return c.JSON(http.StatusOK, response)
+}
+
+func (ctrl *requestController) postRequest(c echo.Context) error {
+	// extract external UUID from the path
+	external_uuid := uuid.FromStringOrNil(c.Param("external_uuid"))
+	if external_uuid == uuid.Nil {
+		return c.String(http.StatusBadRequest, "bad request")
+	}
+
+	// extract event from the body
+	var request requestDTO
+	if err := c.Bind(&request); err != nil {
+		c.Logger().Error(err)
+		return c.String(http.StatusBadRequest, "bad request")
+	}
+
+	// insert event
+	envelope, err := ctrl.store.Insert(c.Request().Context(), external_uuid, request.asEvent(), 0)
+	if err != nil {
+		c.Logger().Error(err)
+		if err == events.DuplicateEventUUID {
+			return c.String(http.StatusConflict, "duplicate event")
+		}
+		return c.String(http.StatusInternalServerError, "failed to insert new event")
+	}
+
+	response := newResponseDTO(envelope)
+	return c.JSON(http.StatusCreated, response)
 }
 
 // representation of the request during HTTP transfer
